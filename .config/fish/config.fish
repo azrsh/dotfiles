1079e4e386982abd4b9f25879972dc803e77177a
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2020-09-04 07:13:00 +0900

    add db alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set PATH /home/azarashi2931/.local/bin $PATH
 alias ojt="oj test -c "./a.out" -d ./tests/"
 alias cm="g++ main.cpp"
 alias mt="cm;ojt"
+alias db="g++ -g -O0 main.cpp;gdb a.out"
 alias acs="acc s"
 alias accs="acc s"
 alias ac="acc"
