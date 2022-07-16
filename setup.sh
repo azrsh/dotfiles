67cbdfee12bbb34db1f4d3564a67b435c6063b19
azarashi <me@azr.sh>
2022-07-17 04:47:16 +0900

    Fix branch option

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ CONFIGS=${@:-"$DEFAULT_CONFIGS"}
 BRANCH=${BRANCH:-"master"}
 
 cd ${HOME}
-git clone -b master git@github.com:azarashi2931/dotfiles.git
+git clone -b ${BRANCH} git@github.com:azarashi2931/dotfiles.git
 cd dotfiles
 bash setup_local.sh $CONFIGS
