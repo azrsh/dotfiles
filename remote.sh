43bf33c71553a98ca64c5137ed175c9869b73770
azarashi <me@azr.sh>
2022-07-17 13:52:55 +0900

    Fix clone method

--- remote.sh
+++ remote.sh
@@ -7,6 +7,6 @@ CONFIGS=${@:-"$DEFAULT_CONFIGS"}
 BRANCH=${BRANCH:-"master"}
 
 cd ${HOME}
-git clone -b ${BRANCH} git@github.com:azarashi2931/dotfiles.git
+git clone -b ${BRANCH} https://github.com/azarashi2931/dotfiles.git
 cd dotfiles
 stow -v --no-folding ${CONFIGS}
