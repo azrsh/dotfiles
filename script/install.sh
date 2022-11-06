7d87c9fa0f75ee2d924b42e6e65c695da280dbef
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2022-11-07 00:24:20 +0900

    Add auto chmod

--- script/install.sh
+++ script/install.sh
@@ -2,8 +2,21 @@
 set -euo pipefail
 
 CONFIGS=${@}
-stow -v --no-folding -d ./config -t ${HOME} ${CONFIGS}
 
-mkdir -p ./bin
-stow -v --no-folding -d ./tool -t ./bin $(find ./tool/* -type d -exec basename {} \;)
-echo "Please add '$(pwd)/bin' to \$PATH"
+function config() {
+  stow -v --no-folding -d ./config -t ${HOME} ${CONFIGS}
+}
+
+function tool() {
+  mkdir -p ./bin
+  stow -v --no-folding -d ./tool -t ./bin $(find ./tool/* -type d -exec basename {} \;)
+
+  local binaries="$(find ./bin/* -type l)"
+  if [ -n "${binaries}" ]; then
+    chmod +x $binaries
+  fi
+  echo "Please add '$(pwd)/bin' to \$PATH"
+}
+
+config
+tool
--- script/update.sh
+++ script/update.sh
@@ -5,7 +5,8 @@ CONFIGS=${@}
 
 script/uninstall.sh ${CONFIGS}
 
-git pull origin HEAD
+git pull origin "$(git branch --show-current)"
+git submodule foreach git reset --hard HEAD # Discard local changes
 git submodule update
 
 script/install.sh ${CONFIGS}
