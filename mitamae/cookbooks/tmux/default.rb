61e33b831267e52ba018412235524b3c456c6b4f
azrsh <46126763+azrsh@users.noreply.github.com>
2025-04-07 01:10:28 +0900

    mitamae: Support Linux in the tmux recipe

--- mitamae/cookbooks/tmux/default.rb
+++ mitamae/cookbooks/tmux/default.rb
@@ -1,4 +1,17 @@
-package "tmux"
+if node[:platform] == "darwin" then
+  package "tmux"
+elsif node[:platform] == "ubuntu" then
+  package "tmux" do
+    user "root"
+  end
+elsif node[:platform] == "arch" then
+  package "tmux" do
+    user "root"
+  end
+else
+  MItamae.logger.error "unsupported platform #{node[:platform]}: #{__FILE__}:#{__LINE__}"
+  exit 1
+end
 
 home = run_command("printenv HOME").stdout.strip
 config_file_path = ".tmux.conf"
