f9850d9bd98f5deff41b67d7c3170c534841d0de
azrsh <46126763+azrsh@users.noreply.github.com>
2025-04-06 11:51:01 +0900

    mitamae: Support Linux in the direnv recipe

--- mitamae/cookbooks/direnv/default.rb
+++ mitamae/cookbooks/direnv/default.rb
@@ -1,6 +1,19 @@
 include_recipe "../../custom_resources/add_1_line"
 
-package "direnv"
+if node[:platform] == "darwin" then
+  package "direnv"
+elsif node[:platform] == "ubuntu" then
+  package "direnv" do
+    user "root"
+  end
+elsif node[:platform] == "arch" then
+  package "direnv" do
+    user "root"
+  end
+else
+  MItamae.logger.error "unsupported platform #{node[:platform]}: #{__FILE__}:#{__LINE__}"
+  exit 1
+end
 
 home = run_command("printenv HOME").stdout.strip
 add_1_line "#{home}/.zshenv" do
