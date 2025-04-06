383fa913abc71bafac0a95657b1102432cd5b055
azrsh <46126763+azrsh@users.noreply.github.com>
2025-04-07 01:09:00 +0900

    mitamae: Support Linux in the gnuutils recipe

--- mitamae/cookbooks/gnuutils/default.rb
+++ mitamae/cookbooks/gnuutils/default.rb
@@ -1,10 +1,47 @@
 include_recipe "../../custom_resources/add_1_line"
 
-package "binutils"
-package "coreutils"
-package "diffutils"
-package "findutils"
-package "moreutils"
+if node[:platform] == "darwin" then
+  package "binutils"
+  package "coreutils"
+  package "diffutils"
+  package "findutils"
+  package "moreutils"
+elsif node[:platform] == "ubuntu" then
+  package "binutils" do
+    user "root"
+  end
+  package "coreutils" do
+    user "root"
+  end
+  package "diffutils" do
+    user "root"
+  end
+  package "findutils" do
+    user "root"
+  end
+  package "moreutils" do
+    user "root"
+  end
+elsif node[:platform] == "arch" then
+  package "binutils" do
+    user "root"
+  end
+  package "coreutils" do
+    user "root"
+  end
+  package "diffutils" do
+    user "root"
+  end
+  package "findutils" do
+    user "root"
+  end
+  package "moreutils" do
+    user "root"
+  end
+else
+  MItamae.logger.error "unsupported platform #{node[:platform]}: #{__FILE__}:#{__LINE__}"
+  exit 1
+end
 
 home = run_command("printenv HOME").stdout.strip
 dirpath = ".config/dotfiles/gnuutils"
