d1e499c3a00a54761803c626c130a77c93a2f1a7
azrsh <46126763+azrsh@users.noreply.github.com>
2025-04-25 23:36:11 +0900

    mitamae: Support Linux in the install.sh

--- mitamae/install.sh
+++ mitamae/install.sh
@@ -5,5 +5,6 @@ set -euCo pipefail
 bin/setup
 case "$(uname)" in
   "Darwin")  bin/mitamae local $@ roles/darwin/default.rb ;;
+  "Linux")  bin/mitamae local $@ roles/linux/default.rb ;;
   *) echo "unsupproted platform: $(uname)"; exit 1 ;;
 esac
